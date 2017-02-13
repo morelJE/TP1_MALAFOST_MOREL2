44a09999a6a59a35d046485500025a2d0ef284c3
Antoine Morel <antoine.morel1@grenoble-inp.fr>
2017-02-13 12:30:48 +0100

    correction TP1

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <assert.h>
 #include "Dvector.h"
-using namespace std;
+//using namespace std;
 
 int main()
 {
 
-    Dvector d(3);
-    cout << d.size();
-    cout << "\n";
-    double i = 2;
-    Dvector e(3,&i);
-    e.display(cout);
-    /*Dvector x;
+
+    Dvector v(3,2.5);
+    assert( v.size() == 3 );
+    std::stringstream str;
+    v.display( str );
+    assert( str.str() == "2.5\n2.5\n2.5\n" );
+    std::cout << "OK\n";
+
+
+
+    Dvector x;
     x = Dvector (3, 1.);
     x.display(cout);
-    Dvector x = Dvector (3, 1.);
-    x.display(cout);*/
+    std::cout << "\n";
+    Dvector y = Dvector (3, 1.);
+    y.display(cout);
 
 
 
